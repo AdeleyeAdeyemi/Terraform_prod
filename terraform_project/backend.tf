f27ebd29fc5ef9abbdb2492dfd01154a30a8f332
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-06-28 14:42:41 +0100

    Change Terraform backend from local to S3

--- terraform_project/backend.tf
+++ terraform_project/backend.tf
@@ -1,6 +1,9 @@
 
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
+    backend "s3" {
+        bucket         = var.s3_bucket
+        key            = "terraform.tfstate"
+        region         = var.region
+        dynamodb_table = var.dynamodb_table_name
+    }
 }
