1ab9a6382c348c391911a76149e357c918b6aa56
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-08-08 18:31:32 +0100

    Update backend.tf

--- terraform_project/backend.tf
+++ terraform_project/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-kadel"
     key            = "terraform_project/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile = "true"
