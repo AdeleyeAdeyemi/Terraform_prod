f327fe6fdabf6675c42c18314d4d7214d7eca408
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-08-08 18:11:56 +0100

    Update backend.tf

--- terraform_project/backend.tf
+++ terraform_project/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-terraform-state-bucket"
     key            = "terraform_project/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "use_lockfile"
+    use_lockfile = "terraform-lock-table"
     
   }
 }
