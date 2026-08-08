3d5820b48ab29958f0716e47fb1777747be3be12
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-08-08 16:35:00 +0100

    Update Terraform backend configuration

--- terraform_project/backend.tf
+++ terraform_project/backend.tf
@@ -1,9 +1,8 @@
-
 terraform {
-    backend "s3" {
-        bucket         = var.s3_bucket
-        key            = "terraform.tfstate"
-        region         = var.region
-        dynamodb_table = var.dynamodb_table_name
-    }
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "terraform_project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+  }
 }
